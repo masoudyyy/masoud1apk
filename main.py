74f4fca021404fe34e623ee52558aede3463b922
masoudyyy <36385874+masoudyyy@users.noreply.github.com>
2025-10-29 09:10:52 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+import random
+
+kivy.require('1.9.0')
+
+class MyRoot(BoxLayout):
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
+
+
+class pmo(App):
+    
+    def build(self):
+        return MyRoot()
+
+pmo = pmo()
+pmo.run()
